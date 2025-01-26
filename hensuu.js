9bbfa986cbc93dab358022385de488fd38cc9b6b
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2025-01-26 22:26:04 +0900

    Update hensuu.js

--- hensuu.js
+++ hensuu.js
@@ -1,1 +1,5 @@
-
+var who66 = 'world';
+function sayhello(){ alert('Hello,' + who + '!'); }
+function taro(){ who = '太郎'; }
+function hanako(){ who = '花子'; }
+function gakita(){ alert(who + 'が来た' + '!'); }
