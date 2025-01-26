74253e679a55922031a3271cfab36b4460a9fb4b
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2025-01-26 23:39:35 +0900

    Update gazo.js

--- gazo.js
+++ gazo.js
@@ -1,6 +1,6 @@
 function imgchange1() {
-    document.getElementById('josai').src = './image/computer_note_good.png';
+    document.getElementById('pasokon').src = './image/computer_note_good.png';
 }
 function imgchange2() {
-    document.getElementById('josai').src = './image/computer_note_bad.png';
+    document.getElementById('pasokon').src = './image/computer_note_bad.png';
 }
