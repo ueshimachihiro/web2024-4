c1184554c88e0c325759072b28231185af59cad1
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2025-01-27 00:15:24 +0900

    Update nazo.js

--- nazo.js
+++ nazo.js
@@ -1,1 +1,7 @@
-
+function nazo(){
+  if(document.getElementById('answer').value =='夢')
+  {alert('あたり！');}
+  else if(document.getElementById('answer').value =='ゆめ')
+  {alert('あたり！');}
+  else{alert('はずれ！');}
+}
