7c0374ca279ba3632679c1c6cad8dc693899b415
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2025-01-26 23:07:47 +0900

    Update gazo.js

--- gazo.js
+++ gazo.js
@@ -1,1 +1,6 @@
-
+function imgchange1() {
+    document.getElementById('josai').src = './image/masukottocharacter.jpg';
+}
+function imgchange2() {
+    document.getElementById('josai').src = './image/logo.png';
+}
