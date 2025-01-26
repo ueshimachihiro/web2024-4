37c663cb5ddac8c56803c23b825b9cd14dd21fdc
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2025-01-27 00:16:17 +0900

    Update uruu.js

--- uruu.js
+++ uruu.js
@@ -1,1 +1,8 @@
-
+function uruudosi(){
+ var x = Number(document.getElementById('input5').value);
+ if(x % 100 == 0 && x % 400 > 0)
+  {alert('平年');}
+  else if(x % 4 ==0)
+  {alert('閏年');}
+  else{alert('平年');}
+}
