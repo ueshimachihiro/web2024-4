e3b9e7cb740f41333de59a42f44c624c918dd895
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2025-01-27 00:19:55 +0900

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -2,4 +2,9 @@ function bmi(){
  var x = Number(document.getElementById('input3').value);
  var y = Number(document.getElementById('input4').value);
  alert(y/(x*x));
+ if(y/(x*x) < 18.5)
+  {alert('低体重');}
+  else if(y/(x*x) < 25)
+  {alert('普通');}
+  else{alert('肥満');}
 }
